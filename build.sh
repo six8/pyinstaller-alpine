035522e8cc9ff9626a62fd502fe8fbebb83ad43f
Mike Thornton <six8@devdetails.com>
2017-05-30 10:57:34 -0500

    Fix typo

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export PYINSTALLER_TAG=${1:-v3.3}
-export APLINE_VERSION=${2:-v3.4}
+export ALPINE_VERSION=${2:-v3.4}
 
-REPO="six8/pyinstaller-alpine:apline-${APLINE_VERSION}-pyinstaller-${PYINSTALLER_TAG}"
+REPO="six8/pyinstaller-alpine:alpine-${ALPINE_VERSION}-pyinstaller-${PYINSTALLER_TAG}"
 
 docker build \
     --build-arg PYINSTALLER_TAG=${PYINSTALLER_TAG} \
