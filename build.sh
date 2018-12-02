3d83517b1011d06f5aa2771468b67f3812f2f543
ali <ali@sifirbir.xyz>
2018-12-02 15:55:19 +0000

    make alpine version and repo name optional

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-export PYINSTALLER_TAG=${1:-v3.3}
-export ALPINE_VERSION=${2:-v3.4}
+export PYINSTALLER_TAG=${1:-v3.4}
+export ALPINE_VERSION=${2:-3.6}
+export REPO=${3:-six8/pyinstaller-alpine}
 
-REPO="six8/pyinstaller-alpine:alpine-${ALPINE_VERSION}-pyinstaller-${PYINSTALLER_TAG}"
+REPO="${REPO}:alpine-${ALPINE_VERSION}-pyinstaller-${PYINSTALLER_TAG}"
 
-docker build \
+cat Dockerfile | envsubst | docker build -f - \
     --build-arg PYINSTALLER_TAG=${PYINSTALLER_TAG} \
+    --build-arg ALPINE_VERSION=${ALPINE_VERSION} \
     -t $REPO .
 
 docker push $REPO
