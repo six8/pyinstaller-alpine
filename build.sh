28a788d7001b6aeb07a4dbe5ab3ed6351b41157e
saranyav-pulsesecure <76927379+saranyav-pulsesecure@users.noreply.github.com>
2021-01-12 22:27:28 +0530

    Build pyinstaller-alpine image with alpine 3.12 and python 2.7 (#11)
    * Build Alpine 3.12 with python 2.7
    * Added python2.7 in config

--- build.sh
+++ build.sh
@@ -4,8 +4,9 @@ REPOSITORY=${1:-six8/pyinstaller-alpine-linux-amd64}
 ARCH=${2:-''}
 PYINSTALLER_TAG=${3:-v3.6}
 ALPINE_VERSION=${4:-3.7}
+PYTHON_VERSION=${5:-3.7}
 
-docker build -t ${REPOSITORY}:alpine-${ALPINE_VERSION}-pyinstaller-${PYINSTALLER_TAG} \
+docker build -f python${PYTHON_VERSION}.Dockerfile -t ${REPOSITORY}:alpine-${ALPINE_VERSION}-python-${PYTHON_VERSION}-pyinstaller-${PYINSTALLER_TAG} \
 	--build-arg ARCH=${ARCH} \
 	--build-arg PYINSTALLER_TAG=${PYINSTALLER_TAG} \
 	--build-arg ALPINE_VERSION=${ALPINE_VERSION} \
